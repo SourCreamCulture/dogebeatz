e4171d2bf1a2684b88f4e74c22637155df307347
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-05-21 16:32:49 +0100

    up

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ bot.on("ready", async (user) => {
 
 	bot.editSelf({ avatarUrl: 'https://avatars.githubusercontent.com/u/83242673?s=400&u=78e0a77d196784ca33e981364fda0129b884ec85&v=4' })
 
-	queue = await getQueue();
-	console.log(queue)
+	queue = await getQueue().tojson();
+	console.log({queue})
 
 	const foundRooms = topRooms.filter(
 		(room) => room.creatorId == config.ownerid // Filter for rooms created by a specific user
