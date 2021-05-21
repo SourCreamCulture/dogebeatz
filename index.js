bb33824d37e5bb8c88386053f3fe5b0ea13b57ab
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-05-21 18:55:11 +0100

    up

--- index.js
+++ index.js
@@ -333,7 +333,8 @@ const playFromUrl = async (room, url) => {
 	}
 	if (!stream) return;
 	timer = startTimer(info.videoDetails.lengthSeconds, function () {
-		console.log('a')
+		queue.shift();
+		console.log(queue);
 		//if (!nextInQueue(room)) room.sendChatMessage("Nothing in queue!")
 	})
 	const audioConnection = await room.connect(); // Connect to the room voice server (or grab it, if already connected.)
